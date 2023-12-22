c4cd5fcb84b57d0e0ba3a082742e1b6973736871
Azamjon62 <azamjonx499@gmail.com>
2023-12-22 13:59:05 +0500

    adding static tables

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,36 @@
-
 function Main() {
   return (
     <div>
-        
+        <div className="container">
+            <table className=" border-black border-[1px]">
+                <thead className="border-black border-[1px]" >
+                <tr className="">
+                    <th className=" w-[320px] border-black border-[1px]" >Person</th>
+                    <th className=" w-[200px] mr-[10px] border-black border-[1px]" >Food</th>
+                    <th className="w-[20%] border-black border-[1px]" >Price</th>
+                </tr>
+                </thead>
+                <tbody className="border-black border-[1px]" >
+                    <tr className="border-black border-[1px]" >
+                        <td className="border-black border-[1px]">A'zamjon</td>
+                        <td className="border-black border-[1px]">osh</td>
+                        <td className="border-black border-[1px]">15 000</td>
+                    </tr>
+                    <tr className="border-black border-[1px]" >
+                        <td className="border-black border-[1px]">Jahongir</td>
+                        <td className="border-black border-[1px]">osh</td>
+                        <td className="border-black border-[1px]">20 000</td>
+                    </tr>
+                    <tr className="border-black border-[1px]" >
+                        <td className="border-black border-[1px]">Ayubhon</td>
+                        <td className="border-black border-[1px]">osh</td>
+                        <td className="border-black border-[1px]">20 000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
