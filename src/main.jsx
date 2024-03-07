24347d9e12f40f6ea690cc5ed7f35e59f47403bb
Azamjon62 <azamjonx499@gmail.com>
2024-03-07 12:41:51 +0500

    added firebase api

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,21 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-// function change(x, y) {
-//     y -= x
-//     x = y + x
-//     y = x - y
-
-//   console.log(x, y);
-// }
-
-// change(5, 4)
-
-
-
-  // changeCallback(x)
-  // x = y
-  // function changeCallback(x) {
-  //   y = x
-    
-  // }
+reportWebVitals();
