895d6d883762bf711b8fd0b4c645b7ad87fadc40
Azamjon62 <azamjonx499@gmail.com>
2024-03-07 10:46:42 +0500

    data base connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import Main from "./Components/Main/Main"
-// import FloatingActionButtonZoom from "./pages/action";
+import Main from "./Components/Main/Main";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Make a GET request to fetch data from the backend
+    fetch("otto.db.elephantsql.com")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.message); // Update state with data from the backend
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []); // Empty dependency array ensures useEffect runs only once
+
   return (
     <>
       <main>
-        {/* <FloatingActionButtonZoom/> */}
         <Main />
+        {data ? <p>{data}</p> : null}
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
